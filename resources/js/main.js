afc8e029f848f018febf26dc47c3c7f8b5c0d474
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-03-23 12:25:45 +0600

    Sticky navigation part done

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+
+    // Sticky Menu
+    $(".js-services-section").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    });
+
+    // MIXITUP (Portfolio Section)
     var mixer = mixitup('.container');
 });
