ac9029f4f1d8763e7127e17138305af9d35ddd77
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-03-26 22:10:38 +0600

    Hamburger menu part done

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,6 +9,27 @@ $(document).ready(function() {
         }
     });
 
+    //Active Link
+    $("nav ul li a").click(function()
+    {
+        $("nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("nav a img.logo").click(function()
+    {
+        $("nav ul li a").removeClass("active");
+        $("nav ul li:first-child a").addClass("active");
+    });
+
     // MIXITUP (Portfolio Section)
     var mixer = mixitup('.container');
 });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
